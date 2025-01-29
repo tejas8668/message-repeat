7642239f161f5af435a3b5502f5794b9047b9690
tejas8668 <134821612+tejas8668@users.noreply.github.com>
2025-01-29 21:38:16 +0530

    Update commands.py

--- Plugins/commands.py
+++ Plugins/commands.py
@@ -5,6 +5,12 @@ from pyrogram import filters
 from bot import channelforward
 from config import Config
 from translation import Translation
+from pymongo import MongoClient
+
+# MongoDB क्लाइंट बनाएं
+client = MongoClient(Config.MONGO_URI)
+db = client[Config.MONGO_DB]
+messages_collection = db["messages"]
 
 
 ################################################################################################################################################################################################################################################
@@ -30,3 +36,38 @@ async def about(client, message):
     )
 
 ################################################################################################################################################################################################################################################
+# add command
+
+@channelforward.on_message(filters.command("add") & filters.private & filters.incoming)
+async def add(client, message):
+    await message.reply("Please enter the channel ID you want to index")
+    channel_id = message.text.split(" ")[1]
+    messages = await channelforward.get_messages(int(channel_id), limit=100)
+    for message in messages:
+        message_dict = {
+            "channel_id": channel_id,
+            "message_id": message.id,
+            "message_text": message.text,
+            "message_type": message.type
+        }
+        messages_collection.insert_one(message_dict)
+    await message.reply("Channel messages indexed successfully")
+
+################################################################################################################################################################################################################################################
+# clear command
+
+@channelforward.on_message(filters.command("clear") & filters.private & filters.incoming)
+async def clear(client, message):
+    messages_collection.delete_many({})
+    await message.reply("Stored messages deleted successfully")
+
+################################################################################################################################################################################################################################################
+# clrcnl command
+
+@channelforward.on_message(filters.command("clrcnl") & filters.private & filters.incoming)
+async def clrcnl(client, message):
+    channel_id = message.chat.id
+    messages = await channelforward.get_messages(channel_id, limit=100)
+    for message in messages:
+        await channelforward.delete_messages(channel_id, message.id)
+    await message.reply("Channel messages deleted successfully")
